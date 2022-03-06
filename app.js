14bb67228096592f78be850936e4356f836c0105
Tebs <tebsandro@gmail.com>
2022-03-06 08:53:37 +0300

    Autre méthode

--- app.js
+++ app.js
@@ -34,6 +34,15 @@ for(let i=1; i<=10; i++) {
 }
 console.log("\n\n");
 
+console.log("Autre méthode - Nombre de 1 à 10 où on affiche juste ceux qui sont inférieurs à 5 :");
+for(let i=1; i<=10; i++) {
+    console.log(i);
+    if(i>=4){
+        break;
+    }   
+}
+console.log("\n\n");
+
 console.log("Factoriel de 5 :");
 let result=1;
 for(let i=5; i>1; i--) {
