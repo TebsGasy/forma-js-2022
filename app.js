bf40f73002dea5e972b4e0e45bda03289b7c8d06
Tebs <tebsandro@gmail.com>
2022-03-08 11:51:26 +0300

    Response of TechzaraJavascript exercise

--- app.js
+++ app.js
@@ -49,3 +49,46 @@ for(let i=5; i>1; i--) {
     result*=i;  
 }
 console.log(result);
+console.log("\n\n");
+
+/* Excercice:
+
+Supposons texte iray "Techzara",
+misy text iray atao hoe "Javascript" ohatra.
+Atambatra izy roa ireo, zany hoe "TechzaraJavascript" ary
+misy nombre n iray, manambara ny hiverenany Javascript aorinan'ilay Techzara.
+Raha n < 0 dia ilay Techzara indray no multipliena par n
+
+Ex:
+n = 2 =>> TechzaraJavascriptJavascript
+n = 1 =>> TechzaraJavascript
+n = -2 =>> TechzaraTechzaraJavascript */
+
+
+const text1 = "Techzara";
+const text2 = "Javascript";
+let stringResult = "";
+
+let number=prompt("Veuillez entrer un nombre s'il vous plaît ?");;
+
+if(number==0) {
+    stringResult=text1+text2;
+    console.log(stringResult);
+} else if(number>0) {
+    let tmp="";
+    for(let i=0; i<number; i++) {
+        tmp+=text2;
+    }
+    stringResult=text1+tmp;
+    console.log(stringResult);
+} else if(number<0) {
+    let tmp="";
+    for(let i=number; i<0 ; i++) {
+        tmp+=text1;
+    }
+    stringResult=tmp+text2;
+    console.log(stringResult);
+}
+else {
+    console.log("Veuillez entrer un nombre");
+}
